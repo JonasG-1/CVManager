9ee9128edfc24efa9d81c2835723820291a94c50
Jonas <jonas.gold_@outlook.de>
2025-10-05 01:21:45 +0200

    Initialize Spring Boot backend project with Maven configuration. Add `.gitattributes`, `.gitignore`, application files, and Maven Wrapper.

---
+++ CVManagerBackend/src/main/java/app/goldbach/cvmanagerbackend/CvManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package app.goldbach.cvmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvManagerBackendApplication.class, args);
+    }
+
+}
---
+++ CVManagerBackend/src/test/java/app/goldbach/cvmanagerbackend/CvManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.goldbach.cvmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
