bd675b1952ba94930f12b3ce8bd773f3d412bc38
Jonas <jgoldbach@brockhaus-ag.de>
2025-11-12 15:21:51 +0100

    Add home component, Tailwind CSS, and PrimeNG integration

--- CVManagerFrontend/src/app/app.config.ts
+++ CVManagerFrontend/src/app/app.config.ts
@@ -1,20 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
       }
     })
   ]
---
+++ CVManagerFrontend/src/app/public/home/home.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import {Menubar} from 'primeng/menubar';
+import {ButtonModule} from 'primeng/button';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+  imports: [Menubar, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule, ButtonModule]
+})
+export class Home {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        badge: '3',
+        items: [
+          {
+            label: 'Core',
+            icon: 'pi pi-bolt',
+            shortcut: '⌘+S',
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+            shortcut: '⌘+B',
+          },
+          {
+            separator: true,
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+            shortcut: '⌘+U',
+          },
+        ],
+      },
+    ];
+  }
+}
