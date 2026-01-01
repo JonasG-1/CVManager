a6aca8c7891236ab138da61a65b4fc04a733348b
JonasG-1 <jonas.gold_@outlook.de>
2026-01-01 01:05:54 +0100

    Clean Up imports

--- CVManagerBackend/src/main/java/app/goldbach/cvmanagerbackend/controller/publicapi/PublicProjectController.java
+++ CVManagerBackend/src/main/java/app/goldbach/cvmanagerbackend/controller/publicapi/PublicProjectController.java
@@ -2,10 +2,11 @@ package app.goldbach.cvmanagerbackend.controller.publicapi;
 
 import app.goldbach.cvmanagerbackend.dto.publicapi.PublicProjectDto;
 import app.goldbach.cvmanagerbackend.mapping.ProjectMapper;
-import app.goldbach.cvmanagerbackend.model.Project;
 import app.goldbach.cvmanagerbackend.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
